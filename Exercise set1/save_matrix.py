aae00482b5f1063f78c77db369cddcc5b8ee407d
Ehalkola <149330145+Ehalkola@users.noreply.github.com>
2024-05-12 13:06:59 +0300

    Add files via upload

---
+++ Exercise set1/save_matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array_15x15 = np.arange(1,300)
+matrix_15x15 = array_15x15.reshape(15,15)
+print(matrix_15x15)
+
+relative_path = ('Exercises\save_matrix.py')
+absolut_path = r'C:\Users\Archoste\Coding\Courses\IntroductionToDataAnalytics\Data-Analytics-Exercises\Exercises'
+
